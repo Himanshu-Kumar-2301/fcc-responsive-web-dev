14e30dbbdbe364e5b7cfbbbf7302935042b7f6cd
Himanshu <himanshu.kumar.23001@gmail.com>
2026-04-17 12:15:40 +0530

    add HTML and CSS project display functionality with load more buttons

---
+++ script.js
@@ -0,0 +1,33 @@
+const htmlProjects = [
+  {
+    name: "Fcc Curriculum Outline",
+    description: "Description...",
+    src: "./html-projects/basic-html/curriculum-outline.html",
+  },
+];
+const cssProjects = [];
+const showcaseProjects = [];
+
+const htmlProjectsContainer = document.querySelector("#html .container");
+const cssProjectsContainer = document.querySelector("#css .container");
+const showcaseProjectsContainer = document.querySelector("#shocase .container");
+
+let numberOfCards = 3;
+
+if (window.innerWidth >= 550 && window.innerWidth <= 800) {
+  numberOfCards = 4;
+}
+
+const showCards = (projectList, element) => {
+  element.innerHTML = "";
+  projectList.forEach((project) => {
+    element.innerHTML += `<div class="card">
+        <h3>${project.name}</h3>
+        <iframe src="${project.src}" width="100%" height="200"></iframe>
+        <p>${project.description}</p>
+        <a target="_blank" href="${project.src}">View Project</a>
+    </div>`;
+  });
+};
+
+showCards(htmlProjects, htmlProjectsContainer);
